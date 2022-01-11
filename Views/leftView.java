244938e49236e8bb66e7bb13596d48644315f2c7
tricky_coder <61200794+chaithra-ac@users.noreply.github.com>
2022-01-11 15:52:51 +0530

    Create leftView.java

---
+++ Views/leftView.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+            List<Integer>l=new ArrayList<>();
+    public List<Integer> leftSideView(TreeNode root) {
+          compute(root,0); 
+          return l;
+    }
+    private void compute(TreeNode root,int level){
+        if(root==null)
+            return ;
+        if(level==l.size()){
+            l.add(root.val);
+        }
+       compute(root.left,level+1);
+       compute(root.right,level+1);
+    }
+}
