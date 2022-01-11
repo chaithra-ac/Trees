3d96af6cd50bb6a7ae503d989be8dd89628db475
tricky_coder <61200794+chaithra-ac@users.noreply.github.com>
2022-01-11 11:04:36 +0530

    Create postOrder.java

---
+++ Traversal/Recursion/postOrder.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+            List<Integer>l=new ArrayList<>();
+    public List<Integer> inorderTraversal(TreeNode root) {
+        if(root==null){
+            return new ArrayList();
+        }
+        inorderTraversal(root.left);
+        inorderTraversal(root.right);
+        l.add(root.val);
+        return l;
+        
+    }
+}
